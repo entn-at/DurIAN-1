7223a7e4e1ff4515782743fd18c012bbc3637e1a
xcmyz <xcmyz@outlook.com>
2020-06-12 14:56:53 +0800

    update hyper parameter

--- hparams.py
+++ hparams.py
@@ -1,9 +1,9 @@
 # Mel
 num_mels = 80
 num_freq = 1025
-sample_rate = 24000
+sample_rate = 22050
 frame_length_ms = 50
-frame_shift_ms = 10
+frame_shift_ms = 12.5
 preemphasis = 0.97
 fmin = 40
 min_level_db = -100
